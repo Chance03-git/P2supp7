cce1bc331263d343c83786388607afcb271d30f3
Chance03-git <Chancefoster05@gmail.com>
2024-12-06 22:15:53 -0500

    function defined for append to file

--- FileAppender.java
+++ FileAppender.java
@@ -1,6 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileAppender {
+     public static void appendToFile(String filename, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.write(content);
+            writer.newLine(); // Add a newline after appending the content
+        }
+    }
     public static void main(String[] args) {
         String filename = "example.txt"; // File to append to
         String content = "This is additional content."; // Content to append
